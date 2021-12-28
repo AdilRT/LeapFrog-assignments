96dd4832614d0bc00a7a60ae16d8af62e2521e47
AdilRT <adiltuladhar77@gmail.com>
2021-12-28 18:28:47 +0545

    2: solved playerCar overlay

--- JS/Car-lane/src/game.js
+++ JS/Car-lane/src/game.js
@@ -1,19 +1,18 @@
 //setup canvas
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 500;
+canvas.width = 510;
 canvas.height = 600;
-ctx.globalCompositeOperation='source-over';
-
 
 let speed = 10
-let playerCarPositionX= canvas.width/2-25;
-let playerCarPositionY= canvas.height - 80;
-let playerCarWidth = 50;
-let playerCarHeight = 80;
 
-let possibleObstaclePosition = [((canvas.width/3)-(playerCarWidth/2)),((canvas.width/3)*2-(playerCarWidth/2)),((canvas.width/3)*3-(playerCarWidth/2))];
+let playerCarWidth = 80; 
+let playerCarHeight = 120;
+let playerCarPositionX= (canvas.width/2)-(playerCarWidth/2);
+let playerCarPositionY= canvas.height - playerCarHeight;
 
+let possibleObstaclePosition = [45,215,385];
+// let possibleObstaclePosition = [80,220,300]
 
 //draw lane
 function drawLane(){
@@ -45,46 +44,49 @@ function drawPlayer () {
     player.src = 'images/player.png';
 
     player.onload=()=>{
-        ctx.drawImage(player,playerCarPositionX,playerCarPositionY,playerCarWidth,playerCarHeight);
+        let a= ()=>{
+            ctx.drawImage(player,playerCarPositionX,playerCarPositionY,playerCarWidth,playerCarHeight);
+
+        }
         // ctx.globalCompositeOperation='destination-over';
+        requestAnimationFrame(a);
     }
 }
-drawLane();
-drawPlayer();
+
 
 //draw obstacle
 
 class Obstacle{
     constructor(){
         this.x = getRandomPosition(possibleObstaclePosition);
-        console.log(this.x);
-        this.y = getRandomNumber(-150,-800);
-        this.speed = 10;
+        this.y = 0;
+        this.speed = 4;
     }
 
     drawObstacle(){
         const obstacle = new Image();
         obstacle.src = 'images/player.png';
 
-        obstacle.onload=()=>{
-            let y=0;
+        obstacle.onload =()=>{
             let moveObstacle = () =>{
-                ctx.drawImage(obstacle,this.x,this.y,50,50);
-                y += speed;
+                ctx.drawImage(obstacle,this.x,this.y,playerCarWidth,playerCarHeight);
+                this.y += 10;
                 // console.log(y);
     
-            if(y>=canvas.height) y=0;
+            if(this.y>=canvas.height){
+                this.y=-400;
+                this.x = getRandomPosition(possibleObstaclePosition);
+
+            } 
             //recursively call moveRoad hence infinite loop
             requestAnimationFrame(moveObstacle);
                 
             }
             moveObstacle();
-
-
         }
-
     }
 }
-
+drawLane();
+drawPlayer();
 const obstacle1 = new Obstacle();
 obstacle1.drawObstacle();
